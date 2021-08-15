8ab614f40b555d353c75e5b4c0d899b7ba39b13a
naldal <cec7378@gmail.com>
2021-08-16 01:26:33 +0900

    Initial Commit

---
+++ SwiftUITimer/SwiftUITimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITimerApp.swift
+//  SwiftUITimer
+//
+//  Created by 송하민 on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
